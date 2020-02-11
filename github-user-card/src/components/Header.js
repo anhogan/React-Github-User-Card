5958db59d56beba2f1db1ece8811cf9abbd6077c
Amanda Hogan <ahogan@teamworks.com>
2020-02-11 11:26:50 -0500

    Add animation to header image

--- github-user-card/src/components/Header.js
+++ github-user-card/src/components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import github from '../github.png';
 import { HeaderDiv, HeaderImage, HeaderText } from './StyledComponents';
+import { gsap } from 'gsap';
 
 const Header = () => {
+  const animate = () => {
+    gsap.to('.image', {duration: 3, x: 1200, ease: "back"})
+  }
+
   return (
     <HeaderDiv>
-      <HeaderImage src={github} alt="GitHub logo" />
+      <HeaderImage onMouseEnter={animate} className="image" src={github} alt="GitHub logo" />
       <HeaderText>GitHub User Snapshot</HeaderText>
     </HeaderDiv>
   )
